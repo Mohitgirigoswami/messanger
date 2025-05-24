965cfd41574cf1c94efcf5e898b5f77df9679b5b
Mohitgirigoswami <mohitgiri@gmail.com>
2025-05-24 17:35:10 +0530

    added basic search functionality

--- app/main/routes.py
+++ app/main/routes.py
@@ -8,21 +8,21 @@ def index():
 
 @main_bp.route('/home',methods=["get",'POST'])
 def home():
-    friend = user_friend.query.filter_by(session.get('id'))
+    current_user = Users.query.filter_by(id=session.get('id')).first()
+    has_searched = 0
+    friend = user_friend.query.filter_by(user_id=session.get('id'))
+    friends = []
+    for f in friend:
+        user = Users.query.filter_by(id=f.friend_id).first()
+        if user:
+            friends.append(user)
     username_to_search = " "
     islogin = session.get('islogin')
     if islogin is not True:
         return redirect(url_for('auth.login'))
     if request.method=="POST":
+        has_searched = True
         to_search = request.form.get("username_to_search")
-        search_results = Users.query.filter_by(username=to_search)
-        
-        return render_template('main/home.html',username_to_search=to_search,search_results=search_results,friends=friends,current_user=session.get('username'))
-    return render_template('main/home.html',current_user=session.get('username'),friends=friends)
-
-@main_bp.route('/home/add_friend')
-def add_friend():
-    islogin = session.get('islogin')
-    if islogin is not True:
-        return redirect(url_for('auth.login'))
-    return render_template('main/home.html')
+        search_results = Users.query.filter(Users.username == to_search, Users.id != session.get('id')).all()
+        return render_template('main/home.html',has_searched=has_searched,username_to_search=to_search,search_results=search_results,friends=friends,current_user=session.get('username'))
+    return render_template('main/home.html',has_searched=has_searched,current_user=current_user,friends=friends)
