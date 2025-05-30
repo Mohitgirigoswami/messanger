972cd070fd1ef9db2aff7eda2021e3805b70c944
Mohitgirigoswami <mohitgiri3021@gmail.com>
2025-05-30 17:57:43 +0530

    doing

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from app import create_app, socketio
 from app.models import db
 
-app = create_app()
-
+app_instance = create_app()
+app = app_instance 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
