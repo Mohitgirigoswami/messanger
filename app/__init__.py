3f67a9dde86f6caf3288ee99623eeff52773a6be
Mohitgirigoswami <mohitgiri3021@gmail.com>
2025-05-30 17:28:29 +0530

    done

--- app/__init__.py
+++ app/__init__.py
@@ -5,17 +5,18 @@ from flask_wtf import CSRFProtect
 from .models import db
 from .auth import auth_bp
 from .main import main_bp
-
+import os;
 socketio = SocketIO()  # Create at module level
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = "erjnjrnuergkjerngngjkngkjngnjrngjnjgntrkjg"
-    app.config['WTF_CSRF_SECRET_KEY'] = "thfyjgukizaaqxxzcvnmopuytrewertvtegrwt7tjhttt3h6uzgrv5yef"
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///messanger.db'
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'your_super_secret_fallback_key_for_dev')
+    app.config['WTF_CSRF_SECRET_KEY'] = os.environ.get('WTF_CSRF_SECRET_KEY', 'another_super_secret_fallback_key_for_dev')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'sqlite:///instance/messenger.db'
     app.config["SESSION_TYPE"] = "sqlalchemy"
     app.config["SESSION_SQLALCHEMY"] = db
     app.config["SESSION_SQLALCHEMY_TABLE"] = "sessions"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     csrf = CSRFProtect(app)
     Session(app)
